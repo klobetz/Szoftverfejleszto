70f1921de18efc1ffd4321d730e469b0c1dfe8cb
razso <razso@HP-LAPTOP-00>
2020-09-16 08:59:06 +0200

    teszt

---
+++ 1_nap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_nap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ez s szöveg amit kiírok a képrtnyüre");
+            Console.WriteLine("ez meg egy másik szöveg");
+            Console.ReadLine();
+        }
+    }
+}
