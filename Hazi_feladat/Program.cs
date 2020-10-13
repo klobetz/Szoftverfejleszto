8a42850c3554f76737f99d9913546ae696e2f5cd
Ákos Klobetz <klobetz@gmail.com>
2020-10-13 17:59:56 +0200

    #5: házi szebben

--- Hazi_feladat/Program.cs
+++ Hazi_feladat/Program.cs
@@ -11,6 +11,38 @@ namespace Hazi_feladat
         static void Main(string[] args)
         {
             var sorozat = new Sorozatok();
+           
+            var szam = sorozat.Bekeres();            
+           
+            var list = sorozat.Feltoltes(szam);
+            
+            Console.Clear();
+            
+            Console.WriteLine($"Köszönom! A szám amit megadtál: {szam}");
+
+            //írasd ki a sorozat utolsó elemét
+            Console.WriteLine($"\nAz utolsó eleme: {list.Last()}");
+
+            //írasd ki a sorozat eredményét egy sorba
+            Console.WriteLine($"\nA sorozat elemei:");
+            foreach (var item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+
+            //írasd ki a legnagyobb értékét
+            Console.WriteLine($"\n\nA sorozat legnagyobb eleme: {list.Max()}");
+
+            //írasd ki a legkisebb értékét
+            Console.WriteLine($"\nA sorozat legkisebb eleme: {list.Min()}");
+
+            //rendezd a sorozatot növekvő sorrendbe
+            Console.WriteLine($"\nA sorozat elemi rendezve: ");
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.Write($"{item}, ");
+            }
 
             Console.ReadLine();
         }
--- Hazi_feladat/Sorozatok.cs
+++ Hazi_feladat/Sorozatok.cs
@@ -6,7 +6,12 @@ namespace Hazi_feladat
 {
     public class Sorozatok
     {
-        public Sorozatok()
+        //public Sorozatok()
+        //{
+            
+        //}
+        
+        public int Bekeres()
         {
             Console.Write("Kérek egy egész számot: ");
 
@@ -18,8 +23,12 @@ namespace Hazi_feladat
                 Console.WriteLine("Rossz értéket adtál meg, csak 0-a vagy pozitív egész számot használhatsz!");
                 valasz = Console.ReadLine();
             }
-            
 
+            return szam;
+        }
+
+        public List<int> Feltoltes(int szam)
+        {
             var list = new List<int>();
             var r = new Random();
 
@@ -27,34 +36,8 @@ namespace Hazi_feladat
             {
                 list.Add(r.Next(1, 1001));
             }
-            Console.Clear();
-            
-            Console.WriteLine($"Köszönom! A szám amit megadtál: {szam}");
 
-            //írasd ki a sorozat utolsó elemét
-            Console.WriteLine($"\nAz utolsó eleme: {list.Last()}");
-
-            //írasd ki a sorozat eredményét egy sorba
-            Console.WriteLine($"\nA sorozat elemei:");
-            foreach (var item in list)
-            {
-                Console.Write($"{item}, ");
-            }
-
-            //írasd ki a legnagyobb értékét
-            Console.WriteLine($"\n\nA sorozat legnagyobb eleme: {list.Max()}");
-
-            //írasd ki a legkisebb értékét
-            Console.WriteLine($"\nA sorozat legkisebb eleme: {list.Min()}");
-
-            //rendezd a sorozatot növekvő sorrendbe
-            Console.WriteLine($"\nA sorozat elemi rendezve: ");
-            list.Sort();
-            foreach (var item in list)
-            {
-                Console.Write($"{item}, ");
-            }
-
+            return list;
         }
     }
 }
